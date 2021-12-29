3463d33b5414fa7b394d1cade979b6e7f408a35b
Gvido Glazers <glazers@gvido.net>
2021-12-29 16:51:28 +0100

    Hi hi

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# setup
+# Setup
+
 Bootstraps fresh macos installs on my laptops w/ Ansible
+
+### Backups
+
+```sh
+rclone -v --drive-impersonate EMAIL sync /Volumes/Music/ gdrive:music --progress --drive-keep-revision-forever
+```
---
+++ setup.sh
@@ -0,0 +1,10 @@
+git clone git@github.com:gvido/setup.git ~/.setup
+
+# TODO install xcode automagically
+ssh-keygen -t ecdsa -b 521;
+
+# homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)";
+
+brew install ansible && \
+ansible-galaxy collection install community.general;
